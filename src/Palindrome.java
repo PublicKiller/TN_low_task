1fa3cc9036ac3fc47e3c75c6e76914dd10b0b41e
PublicKiller <ik20033@yandex.ru>
2023-06-27 19:29:32 +0300

    код отформатирован

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,9 +1,11 @@
 public class Palindrome {
     String str;
-    public Palindrome(String input){
+
+    public Palindrome(String input) {
         this.str = input;
     }
-    public boolean check(){
+
+    public boolean check() {
         String reverse = "";
         for (int i = this.str.length() - 1; i >= 0; i--) {
             reverse += this.str.charAt(i);
